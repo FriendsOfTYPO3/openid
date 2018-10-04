e1e215da5d9946894a4e5460eb76441060dd7449
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2018-10-04 15:22:13 +0300

    [TASK] Update compatibility to TYPO3 9.5.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '9.0.0',
+    'version' => '9.5.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.0.0-9.1.99',
-            'setup' => '9.0.0-9.1.99',
+            'typo3' => '9.5.0-9.5.999',
+            'setup' => '9.5.0-9.1.999',
         ],
         'conflicts' => [
             'naw_openid' => '',
