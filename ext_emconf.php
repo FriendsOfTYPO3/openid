ae91da195582ab5048217bc8d964bd23dbcdad4e
Benni Mack <benni@typo3.org>
2020-12-18 15:01:55 +0100

    [TASK] Prepare for version 10.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => false,
-    'version' => '10.0.0',
+    'version' => '10.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.19-10.999.999',
-            'setup' => '8.7.19-10.999.999',
+            'typo3' => '9.5.0-10.999.999',
+            'setup' => '9.5.0-10.999.999',
         ],
         'conflicts' => [
             'naw_openid' => '',
